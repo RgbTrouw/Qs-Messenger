0ece162eed84af072e4fbfdea526f1348feef21d
Rgb Trouw <40927728+RgbTrouw@users.noreply.github.com>
2025-10-15 01:53:53 +0300

    Add files via upload

---
+++ 1.0.2/src/QsMessenger/Source/Windows/FriendsList/NoticeLabelObject.cpp
@@ -0,0 +1,61 @@
+/*   QsMessenger Client v 1.0.2 Instant Messaging Application
+     Copyright (C) 2025  Radu G. Balaban G.
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+#include "Headers/NoticeLabelObject.h"
+#include "ui_NoticeLabelObject.h"
+#include "../../../MainWindow.h"
+
+#include <QTimer>
+
+NoticeLabelObject::NoticeLabelObject(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::NoticeLabelObject)
+{
+    ui->setupUi(this);
+
+    emit createNotice();
+
+    QTimer::singleShot(3000, this, [=](){
+
+     emit closeNotice();
+     this->close();
+
+    });
+
+}
+
+NoticeLabelObject::~NoticeLabelObject()
+{
+    delete ui;
+}
+
+
+void NoticeLabelObject::setLabel(QString label){
+
+    ui->label->setText(label);
+
+}
+
+void NoticeLabelObject::setIcon(int value){
+
+    if(value == 0){
+        ui->img->setPixmap(QPixmap("./Resources/icons/smiley_offline.png"));
+    } else {
+        ui->img->setPixmap(QPixmap("./Resources/icons/smiley.png"));
+
+    }
+
+}
---
+++ 1.0.2/src/QsMessenger/Source/Windows/FriendsList/NotificationsWidget.cpp
@@ -0,0 +1,87 @@
+/*   QsMessenger Client v 1.0.2 Instant Messaging Application
+     Copyright (C) 2025  Radu G. Balaban G.
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+#include "Headers/NotificationsWidget.h"
+#include "ui_NotificationsWidget.h"
+
+    // #include <QDebug>
+
+NotificationsWidget::NotificationsWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::NotificationsWidget)
+{
+
+    ui->setupUi(this);
+
+
+    this->setGeometry(this->geometry().x(), 800, 200,0);
+
+
+}
+
+NotificationsWidget::~NotificationsWidget()
+{
+    delete ui;
+}
+
+void NotificationsWidget::newNotification(QString availability, QString username){
+
+    noticesList.prepend(new NoticeLabelObject());
+
+    connect(noticesList.last(), SIGNAL(createNotice()), this, SLOT(createNotice()));
+    connect(noticesList.last(), SIGNAL(closeNotice()), this, SLOT(closeNotice()));
+
+    if (availability == "0"){
+        noticesList.first()->setLabel(username + " went offline...");
+        noticesList.first()->setIcon(0);
+    }
+    else if (availability == "1") {
+        noticesList.first()->setLabel(username + " went online...");
+        noticesList.first()->setIcon(1);
+    }
+
+
+
+    for(int i =0; i< noticesList.count(); i++){
+        ui->verticalLayout->removeItem(ui->verticalLayout->takeAt(i));
+    }
+
+    ui->verticalLayout->removeItem(verticalSpacer);
+
+    for(int i =0; i< noticesList.count(); i++){
+        ui->verticalLayout->addWidget(noticesList.at(i));
+    }
+
+    ui->verticalLayout->addItem(verticalSpacer);
+
+
+}
+
+
+
+void NotificationsWidget::createNotice(){
+
+    this->setGeometry(this->geometry().x(), (this->geometry().y() - 20), 200, (this->geometry().height() + 20));
+
+}
+
+void NotificationsWidget::closeNotice(){
+
+    this->setGeometry(this->geometry().x(), (this->geometry().y() + 20), 200, (this->geometry().height() - 20));
+
+    if (noticesList.count() == 0) {this->close();}
+
+}
