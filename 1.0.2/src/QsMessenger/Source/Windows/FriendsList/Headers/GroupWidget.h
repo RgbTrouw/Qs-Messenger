d690f224aa46de76cb0f90f0e4cb89d8c7c1847f
Rgb Trouw <40927728+RgbTrouw@users.noreply.github.com>
2025-10-15 01:54:40 +0300

    Add files via upload

---
+++ 1.0.2/src/QsMessenger/Source/Windows/FriendsList/Headers/GroupWidget.h
@@ -0,0 +1,94 @@
+/*   QsMessenger Client v 1.0.2 Instant Messaging Application
+     Copyright (C) 2025  Radu G. Balaban G.
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+#ifndef GROUPWIDGET_H
+#define GROUPWIDGET_H
+
+#include "GroupHeaderWidget.h"
+#include "PeerWidget.h"
+
+#include <QWidget>
+#include <QEvent>
+#include <QIcon>
+
+namespace Ui {
+class GroupWidget;
+}
+
+class GroupWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit GroupWidget(QWidget *parent = nullptr);
+    ~GroupWidget();
+
+
+    QString plain_group_data;
+    QList < PeerWidget *> peers;
+    QString group_name;
+
+    GroupHeaderWidget *groupHeader = new GroupHeaderWidget();
+
+    int group_list_position_index;
+
+    int peers_online;
+    int peers_total;
+
+    QString virtualDirection = "up";
+
+    QIcon item_icon;
+
+public slots:
+
+
+    void set_group_name(QString name);
+    void set_plain_data(QString data);
+
+    void clearPeersDisplay();
+    void changePeersDisplay(QString currentType);
+
+    void updateUsersOnline();
+
+
+    QString get_group_name();
+    int usersOnlineCount();
+
+    void addNewUser();
+    void addNewGroup();
+    void removeGroup();
+
+    void appendLastPeer();
+
+
+    void hover(bool value);
+    bool eventFilter(QObject *object, QEvent *event);
+
+signals:
+
+    void addToGroupSignal(QString groupName);
+    void addNewGroupSignal();
+    void removeGroupSignal(QString groupName);
+
+private:
+    Ui::GroupWidget *ui;
+    PeerWidget *pw = new PeerWidget();
+
+
+
+};
+
+#endif // GROUPWIDGET_H
---
+++ 1.0.2/src/QsMessenger/Source/Windows/FriendsList/Headers/NotificationsWidget.h
@@ -0,0 +1,53 @@
+/*   QsMessenger Client v 1.0.2 Instant Messaging Application
+     Copyright (C) 2025  Radu G. Balaban G.
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+#ifndef NOTIFICATIONSWIDGET_H
+#define NOTIFICATIONSWIDGET_H
+
+#include "NoticeLabelObject.h"
+
+#include <QSpacerItem>
+#include <QWidget>
+#include <QTimer>
+#include <QList>
+
+
+namespace Ui {
+class NotificationsWidget;
+}
+
+class NotificationsWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit NotificationsWidget(QWidget *parent = nullptr);
+    ~NotificationsWidget();
+
+    QList<NoticeLabelObject *> noticesList;
+
+public slots:
+    void newNotification(QString availability, QString username);
+    void closeNotice();
+    void createNotice();
+
+private:
+    Ui::NotificationsWidget *ui;
+
+     QSpacerItem *verticalSpacer = new QSpacerItem(0,1000, QSizePolicy::Expanding, QSizePolicy::Expanding);
+};
+
+#endif // NOTIFICATIONSWIDGET_H
