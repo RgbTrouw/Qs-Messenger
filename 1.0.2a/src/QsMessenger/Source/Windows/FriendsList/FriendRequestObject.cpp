be212d1d0290dd49b3d35b333875ce7084268fd4
Rgb Trouw <40927728+RgbTrouw@users.noreply.github.com>
2025-12-26 12:24:42 +0200

    Add files via upload

---
+++ 1.0.2a/src/QsMessenger/Source/Windows/FriendsList/FriendRequestObject.cpp
@@ -0,0 +1,98 @@
+/*   QsMessenger Client v 1.0.2a Instant Messaging Application
+     Copyright (C) 2026  Radu G. Balaban G.
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+#include "Headers/FriendRequestObject.h"
+#include "ui_FriendRequestObject.h"
+
+#include <QRegularExpression>
+#include <QDesktopWidget>
+
+
+FriendRequestObject::FriendRequestObject(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::FriendRequestObject)
+{
+    ui->setupUi(this);
+    ui->addButton->setIcon(QIcon("./Resources/icons/addUser.png"));
+
+    //ui->serverMessageLabel->hide();
+
+    QDesktopWidget dWidget;
+    int screenWidth = dWidget.screen()->width();
+    int screenHeight = dWidget.screen()->height();
+
+    this->setGeometry((screenWidth/2)-(this->width()/2),(screenHeight/2)-(this->height()/2),this->width(),this->height());
+
+    connect(ui->addButton, SIGNAL(clicked()), this, SLOT(add()));
+}
+
+FriendRequestObject::~FriendRequestObject()
+{
+    delete ui;
+}
+
+void FriendRequestObject::add()
+{
+    QString sp = "*@*.*";
+    //match.setPatternSyntax(QRegExp::Wildcard);
+
+    QString w =  QRegularExpression::wildcardToRegularExpression("*@*.*");
+    QRegularExpression re(w);
+    QRegularExpressionMatch match = re.match(ui->lineEdit->text());
+    bool hasMatch = match.hasMatch();
+
+    if (hasMatch){
+
+    email = ui->lineEdit->text();
+
+    ui->serverMessageLabel->setText("  Processing...");
+    emit addNewUser();
+
+    } else {
+        ui->serverMessageLabel->setText("  Email is not validated...");
+    }
+    //st->singleShot(1000, this, SLOT(close_window()));
+}
+
+void FriendRequestObject::feedback(QString message){
+
+
+    //this->setGeometry(this->geometry().x(),this->geometry().y(), this->geometry().width(), this->geometry().height() + 20);
+    ui->serverMessageLabel->show();
+    this->activateWindow();
+
+    ui->serverMessageLabel->setText(message);
+    if(ui->serverMessageLabel->text() == "  Friendship request has been sent..."){
+        st->singleShot(3200, this, SLOT(close_window()));
+    }
+}
+
+void FriendRequestObject::close_window(){
+
+    if (ui->serverMessageLabel->text() == "  Friendship request has been sent..."){
+
+        ui->lineEdit->setText("");
+        ui->serverMessageLabel->setText("");
+        this->close();
+    } else {st->singleShot(3200, this, SLOT(close_window()));}
+
+}
+
+void FriendRequestObject::closeEvent (QCloseEvent *event)
+{
+    ui->lineEdit->setText("");
+    ui->serverMessageLabel->setText("...");
+}
---
+++ 1.0.2a/src/QsMessenger/Source/Windows/FriendsList/FriendshipResponseObject.cpp
@@ -0,0 +1,73 @@
+/*   QsMessenger Client v 1.0.2a Instant Messaging Application
+     Copyright (C) 2026  Radu G. Balaban G.
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+#include "Headers/FriendshipResponseObject.h"
+#include "ui_FriendshipResponseObject.h"
+
+#include <QTimer>
+
+FriendshipResponseObject::FriendshipResponseObject(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::FriendshipResponseObject)
+{
+    ui->setupUi(this);
+    ui->pushButton->setIcon(QIcon("./Resources/icons/addUser.png"));
+
+}
+
+FriendshipResponseObject::~FriendshipResponseObject()
+{
+    delete ui;
+}
+
+void FriendshipResponseObject::on_pushButton_clicked()
+{
+
+    QString rsp = "yes";
+
+    ui->label->setText("  Friendship accepted...");
+
+    st->singleShot(2000, this, SLOT(feedback()));
+
+    emit respond_fr( ui->emailLabel->text(), rsp );
+}
+
+void FriendshipResponseObject::on_pushButton_2_clicked()
+{
+    QString rsp = "no";
+
+    st->singleShot(2000, this, SLOT(feedback()));
+
+    ui->label->setText("  Friendship declined...");
+    emit respond_fr(ui->emailLabel->text(), rsp);
+}
+
+void FriendshipResponseObject::from(QString email){
+
+    ui->emailLabel->setText(email);
+}
+
+void FriendshipResponseObject::feedback(){
+
+    ui->emailLabel->setText("");
+    ui->label->setText("");
+    this->close();
+}
+
+void FriendshipResponseObject::closeEvent (QCloseEvent *event)
+{
+
+}
