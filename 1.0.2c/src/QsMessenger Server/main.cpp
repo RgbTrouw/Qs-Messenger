db43c71c83a3ad994758b3c9bacd91b69059cd5c
Rgb Trouw / Rgb Trust <40927728+RgbTrouw@users.noreply.github.com>
2026-09-10 15:17:30 +0300

    Add files via upload

---
+++ 1.0.2c/src/QsMessenger Server/main.cpp
@@ -0,0 +1,44 @@
+/*   QsMessenger Server v 1.0.2b Rev 4 Instant Messaging Application
+     Copyright (C) 2026  Radu G. Balaban G.
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+
+#include <QtCore/QCoreApplication>
+#include "QsMessengerServer.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    quint16 port = 7080;
+
+    bool log = false;
+    bool verbose = false;
+
+    for(int i=0; i< argc; i++){
+
+        if( qstricmp(argv[i], "-log") == 0){
+            log = true;
+        }
+        if( qstricmp(argv[i], "-v") == 0){
+            verbose = true;
+        }
+    }
+
+    QsMessengerServer server(port, log, verbose);
+
+    Q_UNUSED(server);
+
+    return a.exec();
+}
