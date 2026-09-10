696ca25bf2971ddc1182fdcad06f18c220bbd3de
Rgb Trouw / Rgb Trust <40927728+RgbTrouw@users.noreply.github.com>
2026-09-10 15:14:41 +0300

    Add files via upload

---
+++ 1.0.2c/src/QsMessenger/Source/OtherWindows/Headers/SettingsObject.h
@@ -0,0 +1,54 @@
+/*   QsMessenger v 1.0.2b Rev 4 Instant Messaging Application
+     Copyright (C) 2026  Radu G. Balaban G.
+
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, either version 3 of the License, or
+     (at your option) any later version.
+
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <https://www.gnu.org/licenses/>. */
+
+#ifndef SETTINGSOBJECT_H
+#define SETTINGSOBJECT_H
+ 
+#include <QWidget>
+#include <QSettings>
+
+namespace Ui {
+class SettingsObject;
+}
+
+class SettingsObject : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit SettingsObject(QWidget *parent = nullptr);
+    ~SettingsObject();
+
+public slots:
+    void setSettings(QString host, QString port);
+
+private slots:
+    void updateHostAddressValue();
+
+    void connectionStack();
+    void generalStack();
+    void privacyStack();
+
+signals:
+    void updateHostAddress(QString host);
+
+private:
+    Ui::SettingsObject *ui;
+
+    QSettings savedSettings;
+};
+
+#endif // SETTINGSOBJECT_H
