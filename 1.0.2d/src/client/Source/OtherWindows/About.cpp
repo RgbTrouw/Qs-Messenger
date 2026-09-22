30327b5b3f74fa1664be0c9e5a5320a5b7eab383
Rgb Trouw / Rgb Trust <40927728+RgbTrouw@users.noreply.github.com>
2026-09-22 07:56:28 +0300

    Refactor About class destructor and formatting

--- 1.0.2d/src/client/Source/OtherWindows/About.cpp
+++ 1.0.2d/src/client/Source/OtherWindows/About.cpp
@@ -24,17 +24,11 @@ About::About(QWidget *parent) :
     ui(new Ui::About)
 {
     ui->setupUi(this);
-
     auto const rec =  QGuiApplication::primaryScreen()->geometry();
     auto const sHeight = rec.height();
     auto const sWidth = rec.width();
-
     this->setGeometry(sWidth / 2 - this->geometry().width() / 2, sHeight / 2 - this->geometry().height() * 2, this->geometry().width(), this->geometry().height());
-
     this->raise();
 }
-
 About::~About()
-{
-    delete ui;
-}
+{ delete ui;}
